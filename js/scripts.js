8d863c0983587284436d2bc5b4ea650e66c22e5e
Pavel Zanchuk <buzzik@yahoo.com>
2019-02-03 15:18:28 -0800

    Add business and user logic

---
+++ js/scripts.js
@@ -0,0 +1,103 @@
+var cheesePrice = 1.50;
+var pepperoniPrice = 2.00;
+var mushroomPrice = 1.90;
+var baconPrice = 2.10;
+var olivePrice = 1.60;
+
+var pizzaIdsetter = 0;
+
+
+function TotalOrder(){
+  this.orders = []
+}
+
+TotalOrder.prototype.addOrder = function(pizza){
+  pizza.id = this.assignId();
+  this.orders.push(pizza);
+}
+
+TotalOrder.prototype.assignId = function() {
+  pizzaIdsetter+=1;
+  return pizzaIdsetter;
+}
+
+TotalOrder.prototype.finalCost = function(pizza) {
+  var price = 0;
+
+}
+
+function Pizza(size, price){
+  this.toppings = [],
+  this.size = size,
+  this.price = price
+
+}
+
+function Topping(name, price){
+  this.name = name,
+  this.price = price
+}
+
+Pizza.prototype.addTopping = function(topping){
+  this.toppings.push(topping);
+  this.wholePizzaprice = this.totalCost();
+}
+
+Pizza.prototype.totalCost = function(){
+  var totalCost = 0;
+  for (var i = 0; i < this.toppings.length; i++) {
+    totalCost+=this.toppings[i].price;
+  }
+  return this.price + totalCost;
+}
+
+var myOrder = new TotalOrder();
+
+$(document).ready(function() {
+  $("#pizza-form").submit(function(event) {
+    event.preventDefault();
+      var size = $("input:radio[name=size]:checked").val();
+
+      var price;
+
+      if (size === "Small"){
+        price = 13;
+      }else if(size === "Medium"){
+        price = 16;
+      }else{
+        price = 19;
+      }
+
+      var toppingToArray =[];
+      $("input:checkbox[name=topping]:checked").each(function(){
+      var topping = $(this).val();
+      toppingToArray.push(topping);
+    });
+
+      var pizza = new Pizza(size, price);
+      myOrder.addOrder(pizza);
+      for (var i = 0; i < toppingToArray.length; i++) {
+        if(toppingToArray[i] === "cheese"){
+          var topping = new Topping("Cheese", cheesePrice)
+          pizza.addTopping(topping);
+        }else if(toppingToArray[i] === "pepperoni"){
+          var topping = new Topping("Pepperoni", pepperoniPrice)
+          pizza.addTopping(topping);
+        }else if(toppingToArray[i] === "bacon"){
+          var topping = new Topping("Bacon", baconPrice)
+          pizza.addTopping(topping);
+        }else if(toppingToArray[i] === "mushroom"){
+          var topping = new Topping("Mushrooms", mushroomPrice )
+          pizza.addTopping(topping);
+        }else{
+          var topping = new Topping("Black Olives", olivePrice)
+          pizza.addTopping(topping);
+        }
+      }
+
+        console.log(myOrder);
+    $(".order-info").show();
+    $("#total").html("Your total cost is:<br>$"+ pizza.totalCost().toFixed(2));
+
+  });
+});
